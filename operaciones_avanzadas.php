b1006a103518a7586ff523ba9f775520fb872876
jagoez23 <desarrolloagoez23@gmail.com>
2023-12-21 09:35:06 -0500

    operactiones avanced php

---
+++ operaciones_avanzadas.php
@@ -0,0 +1,14 @@
+<?php
+
+//O functie care primeste un numar si returneaza suma tuturor numerelor in ordine naturala, pana la acel numar inclusiv
+//Fie n nr dat, n>0. Functia adunare: 1+2+3+..+n-1+n
+// Rezolvare : formula de calcul a sumei unui sir este : S = n(n+1)/2
+//
+$numar = 10;
+function suma($numar)
+{
+    $suma = $numar * ($numar + 1) / 2;
+    return $suma;
+}
+$suma_mea = suma($numar);
+print $suma_mea;
